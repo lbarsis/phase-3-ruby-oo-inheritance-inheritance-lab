effe4ddde364ea9bf4d875743ad1aa6cfb8d5ebd
Levi Barsis <lbarsis@gmail.com>
2022-12-02 16:00:34 -0800

    Done with assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require 'pry'
+require 'user.rb'
+
+class Student < User
+  attr_accessor :knowledge
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
 
 end
+
+steve = Student.new
+steve.learn('knowledge')
+
+binding.pry
